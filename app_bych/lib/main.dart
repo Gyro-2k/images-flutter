d8ad5945447ea8af5aa8f1e528f64360ab8e1e84
D Gourisankar Mohanty <dgourisankarmohanty@gmail.com>
2021-06-16 01:24:00 +0530

    Create main.dart
    Took all the code from main.dart file and divided it into different files in the source folder and imported them back into our main dart file

--- app_bych/lib/main.dart
+++ app_bych/lib/main.dart
@@ -6,51 +6,11 @@
 //
 //this is to import code from a third party package
 import 'package:flutter/material.dart';
+//
+//this is to import files from our local system
+import 'source/appbar.dart';
 
 //A 'main' function, always
 void main() {
-//
-//create a text widget to show
-//some text on screen
-  var myapp = MaterialApp(
-    theme: ThemeData.dark(),
-    home: Scaffold(
-      //
-      //floating action button
-      floatingActionButton: FloatingActionButton(
-        //
-        //child widget to show an icon within
-        //the floating action button
-        child: Icon(Icons.add_photo_alternate),
-
-        //this is what happens when you press the button
-        onPressed: () {},
-      ),
-
-      //total top app bar
-      appBar: AppBar(
-        //shows a menu icon on the left
-        //side of the appbar
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {},
-        ),
-
-        //display main text of the appbar
-        title: Text('See some photos, Bych!'),
-
-        //display search icon on right side of appbar
-        //with a bit of padding on the right
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 20.0),
-            child: Icon(Icons.search),
-          ),
-        ],
-      ),
-    ),
-  );
-
-//take the made widget and show it on screen
-  runApp(myapp);
+  runApp(newApp());
 }
